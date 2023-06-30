1925bd980565a76ef76e8d74e4062c1167320770
Augusto Diaz <augustodiaz_1@hotmail.com>
2023-06-30 18:00:28 +1000

    dart

---
+++ dart_language/09_abstract_class.dart
@@ -0,0 +1,54 @@
+void main() {
+  final windPlant = WindPlant(initialEnergy: 100);
+  final nuclearPlant = NuclearPlant(energyLeft: 1000);
+
+  print('wind: ${chargePhone(windPlant)}');
+  print('nuclear: ${chargePhone(nuclearPlant)}');
+}
+
+double chargePhone(EnergyPlant plant) {
+  if (plant.energyLeft < 10) {
+    throw Exception('Not enough energy');
+  }
+
+  return plant.energyLeft - 10;
+}
+
+enum PlantType { nuclear, wind, water }
+
+// Abstract class
+abstract class EnergyPlant {
+  double energyLeft;
+  final PlantType type; // nuclear, wind, water
+
+  EnergyPlant({required this.energyLeft, required this.type});
+
+  void consumeEnergy(double amount);
+}
+
+// extends. Usually you use it when you need to use the whole functionality of the class.
+class WindPlant extends EnergyPlant {
+  WindPlant({required double initialEnergy})
+      : super(energyLeft: initialEnergy, type: PlantType.wind);
+
+  @override
+  void consumeEnergy(double amount) {
+    energyLeft -= amount;
+  }
+}
+
+// implements. Usually you will use it when you need to implement one or more methods but not the whole class.
+class NuclearPlant implements EnergyPlant {
+  @override
+  double energyLeft;
+
+  @override
+  final PlantType type = PlantType.nuclear;
+
+  NuclearPlant({required this.energyLeft});
+
+  @override
+  void consumeEnergy(double amount) {
+    energyLeft -= (amount * 0.5);
+  }
+}
